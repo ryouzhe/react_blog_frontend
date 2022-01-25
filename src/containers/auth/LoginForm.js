7cf6fdbb8a9a0f02431bcb92f30d0c6ed1e9b542
ryouzhe <sjh3183@naver.com>
2022-01-25 16:19:42 +0900

    로그인/회원가입 에러 처리

--- src/containers/auth/LoginForm.js
+++ src/containers/auth/LoginForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { changeField, initializeForm, login } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
@@ -6,6 +6,7 @@ import { check } from '../../modules/user';
 import { useNavigate } from 'react-router-dom';
 
 const LoginForm = () => {
+    const [error, setError] = useState(null);
     const dispatch = useDispatch();
     const { form, auth, authError, user } = useSelector(({auth, user}) => ({
         form: auth.login,
@@ -41,6 +42,7 @@ const LoginForm = () => {
         if(authError) {
             console.log('오류 발생');
             console.log(authError);
+            setError('로그인 실패');
             return;
         }
         if(auth) {
@@ -62,6 +64,7 @@ const LoginForm = () => {
             form={form}
             onChange={onChange}
             onSubmit={onSubmit}
+            error={error}
         />
     );
 };
