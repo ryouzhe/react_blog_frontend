2b689035a54e7510ce78e5aed6d415fbbcc9911c
ryouzhe <sjh3183@naver.com>
2022-01-25 10:56:10 +0900

    project env setting
    >>>>>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import PostList from './pages/PostList';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import WritePage from './pages/WritePage';
+import PostPage from './pages/PostPage';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <Routes>
+        <Route element={<PostList />} path='/' />
+        <Route element={<PostList />} path='/@:username' /> 
+        <Route element={<LoginPage />} path='/login' />
+        <Route element={<RegisterPage />} path='/register' />
+        <Route element={<WritePage />} path='/write' />
+        <Route element={<PostPage />} path='/@:username/:postId' />
+      </Routes>
+    </>
+  )
+};
 
 export default App;
