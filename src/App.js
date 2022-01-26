9a01e60a927e2388cdd8f800b84cd06bdc19da01
ryouzhe <sjh3183@naver.com>
2022-01-26 22:28:47 +0900

    Post List UI 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import PostListPage from './pages/PostList';
+import PostListPage from './pages/PostListPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import WritePage from './pages/WritePage';
