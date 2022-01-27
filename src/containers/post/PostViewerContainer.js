1f4ae82a6a20a0a4c504a11ce8bcb876d611e2b1
ryouzhe <sjh3183@naver.com>
2022-01-27 13:43:27 +0900

    포스트 삭제 기능구현

--- src/containers/post/PostViewerContainer.js
+++ src/containers/post/PostViewerContainer.js
@@ -2,9 +2,10 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams, useNavigate } from 'react-router-dom';
 import { readPost, unloadPost } from '../../modules/post';
-import PostViewer from '../../components/posts/PostViewer';
-import PostActionButtons from '../../components/posts/PostActionButtons';
+import PostViewer from '../../components/post/PostViewer';
+import PostActionButtons from '../../components/post/PostActionButtons';
 import { setOriginalPost } from '../../modules/write';
+import { removePost } from '../../lib/api/posts';
 
 const PostViewerContainer = () => {
     const { postId } = useParams();
@@ -28,13 +29,21 @@ const PostViewerContainer = () => {
         dispatch(setOriginalPost(post));
         navigate('/write');
     }
+    const onRemove = async () => {
+        try {
+            await removePost(postId);
+            navigate('/');
+        } catch (e) {
+            console.log(e);
+        }
+    }
     const ownPost = (user && user._id) === (post && post.user._id);
 
     return <PostViewer 
         post={post} 
         loading={loading} 
         error={error} 
-        actionButtons={ownPost && <PostActionButtons onEdit={onEdit}/>}    
+        actionButtons={ownPost && <PostActionButtons onEdit={onEdit} onRemove={onRemove}/>}    
     />;
 };
 
