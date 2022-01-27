2ff7cb5635a1deb3ad510527747938772eb0090b
ryouzhe <sjh3183@naver.com>
2022-01-27 13:05:27 +0900

    포스트 수정 기능구현

--- src/containers/post/PostViewerContainer.js
+++ src/containers/post/PostViewerContainer.js
@@ -1,16 +1,19 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { readPost, unloadPost } from '../../modules/post';
 import PostViewer from '../../components/posts/PostViewer';
+import PostActionButtons from '../../components/posts/PostActionButtons';
+import { setOriginalPost } from '../../modules/write';
 
 const PostViewerContainer = () => {
     const { postId } = useParams();
     const dispatch = useDispatch();
-    const { post, error, loading } = useSelector(({ post, loading }) => ({
+    const { post, error, loading, user } = useSelector(({ post, loading, user }) => ({
         post: post.post,
         error: post.error,
         loading: loading['post/READ_POST'],
+        user: user.user,
     }));
 
     useEffect(() => {
@@ -20,7 +23,19 @@ const PostViewerContainer = () => {
         };
     }, [dispatch, postId]);
 
-    return <PostViewer post={post} loading={loading} error={error} />;
+    const navigate = useNavigate();
+    const onEdit = () => {
+        dispatch(setOriginalPost(post));
+        navigate('/write');
+    }
+    const ownPost = (user && user._id) === (post && post.user._id);
+
+    return <PostViewer 
+        post={post} 
+        loading={loading} 
+        error={error} 
+        actionButtons={ownPost && <PostActionButtons onEdit={onEdit}/>}    
+    />;
 };
 
 export default PostViewerContainer;
